59a093b599528ff2159dc02cb799a3450cd0e74d
pyyoek <joelnrlson@gmai.com>
2024-10-10 14:38:53 +0300

    rearrangeed the dates

--- resources/views/dates.blade.php
+++ resources/views/dates.blade.php
@@ -4,22 +4,59 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-
     <title>Document</title>
+    <style>
+        .date-row {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+        }
+        .date-item {
+            margin: 10px;
+        }
+        .content {
+            display: none;
+        }
+    </style>
 </head>
 <body>
     <main>
         <center>
+            <h1>
+                <a href="/adat">Add New Attendance</a>&nbsp&nbsp&nbsp 
+                <a href="/add">Add New Student</a>&nbsp&nbsp&nbsp 
+                <a href="/dates">Dates</a>
+            </h1>
 
-        <h1><a href="/adat">Add New attendance</a>&nbsp&nbsp&nbsp <a href="/add">Add New Student</a>&nbsp&nbsp&nbsp <a href="/dates">Dates</a></h1>
+            <div class="date-row">
+                @foreach ($dateTables as $index => $table)
+                    <div class="date-item">
+                        <button onclick="showContent({{ $index }})" class="btn btn-primary">{{ $table }}</button>
+                    </div>
+                @endforeach
+            </div>
 
-        <div>
-            @foreach ($dateTables as $table)
-                <h1>{{$table}}</h1>
+            @foreach ($dateTables as $index => $table)
+                <div id="content-{{ $index }}" class="content">
+                    <h3>Content for {{ $table }}</h3>
+                    <p>This is the content for {{ $table }}.</p>
+                </div>
             @endforeach
-        </div>
         </center>
     </main>
-        
+
+    <script>
+        function showContent(index) {
+            // Hide all content
+            let contents = document.querySelectorAll('.content');
+            contents.forEach(function(content) {
+                content.style.display = 'none';
+            });
+
+            // Show the selected content
+            let selectedContent = document.getElementById('content-' + index);
+            selectedContent.style.display = 'block';
+        }
+    </script>
 </body>
 </html>
